9ac1a864b8849505b0cc8cc00de6471c656a3285
dahead@mailbox.org <dahead@mailbox.org>
2025-06-05 11:09:50 +0200

    Windows build and debug tools added.

---
+++ build-all.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# For Windows
+GOOS=windows GOARCH=amd64 go build -o notes-app.exe
+
+# For Linux
+GOOS=linux GOARCH=amd64 go build -o notes-app
---
+++ debug.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+NOTESAPP_DEBUG=1 ./notes-app
+
